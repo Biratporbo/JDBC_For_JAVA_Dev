05b4d3f90c205e49a6febc945d0db0619b13402f
Birat <biratdey200311@gmail.com>
2026-07-30 22:09:17 +0530

    feat: Add unit test for ExampleMain to ensure it runs without exceptions

---
+++ examples/h2-example/src/test/java/com/example/h2/ExampleMainTest.java
@@ -0,0 +1,11 @@
+package com.example.h2;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleMainTest {
+
+    @Test
+    void runsWithoutException() {
+        ExampleMain.main(new String[0]);
+    }
+}
